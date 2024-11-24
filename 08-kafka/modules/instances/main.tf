c931603037c9eee5b4ef0d6ebf613f7d2ca7d0cd
kntrbs76 <aleksey.stolbov@rt.ru>
2024-11-24 16:07:23 +0300

    full

--- 08-kafka/modules/instances/main.tf
+++ 08-kafka/modules/instances/main.tf
@@ -29,7 +29,7 @@ resource "yandex_compute_instance" "instances" {
   dynamic "secondary_disk" {
     for_each = var.secondary_disk
     content {
-      disk_id     = lookup(secondary_disk.value, "disk_id")
+      disk_id = lookup(secondary_disk.value, "disk_id")
       auto_delete = lookup(secondary_disk.value, "auto_delete", true)
       #auto_delete = var.disk_auto_delete
       #mode        = var.disk_mode
@@ -47,42 +47,15 @@ resource "yandex_compute_instance" "instances" {
   dynamic "network_interface" {
     for_each = var.network_interface
     content {
-      subnet_id      = lookup(network_interface.value, "subnet_id")
-      nat            = lookup(network_interface.value, "nat", false)
+      subnet_id = lookup(network_interface.value, "subnet_id")
+      nat = lookup(network_interface.value, "nat", false)
       #ip_address     = lookup(network_interface.value, "ip_address", var.internal_ip_address)
       #nat_ip_address = lookup(network_interface.value, "nat_ip_address", var.nat_ip_address)
     }
   }
 
   metadata = {
-    ssh-keys           = "${var.vm_user}:${file(var.ssh_public_key)}"
+    ssh-keys = "${var.vm_user}:${file(var.ssh_public_key)}"
   }
 
   allow_stopping_for_update = var.allow_stopping_for_update
-/*
-  provisioner "remote-exec" {
-    inline = ["echo 'Wait until SSH is ready'"]
-
-    connection {
-      host        = self.network_interface[0].nat_ip_address
-      type        = "ssh"
-      user        = local.user
-      private_key = file(local.ssh_private_key)
-    }
-  }
-
-  provisioner "local-exec" {
-    command = "ansible-playbook -u '${local.user}' --private-key '${local.ssh_private_key}' --become -i '${self.network_interface[0].nat_ip_address},' provision.yml"
-  }
-*/
-}
-/*
-resource "local_file" "inventory_file" {
-  content = templatefile("./templates/inventory.tpl",
-    {
-      instances = yandex_compute_instance.instances
-    }
-  )
-  filename = "./inventory.ini"
-}
-*/
